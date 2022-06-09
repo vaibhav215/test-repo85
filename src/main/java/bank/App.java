e05435cd85760f612c44a6e40c164d2da0b0f254
vaibhav215 <102907232+vaibhav215@users.noreply.github.com>
2022-06-09 11:19:14 +0530

    cicd

--- src/main/java/bank/App.java
+++ src/main/java/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "test commit for web-hook" );
+        System.out.println( "test commit for CICD web-hook" );
     }
 }
